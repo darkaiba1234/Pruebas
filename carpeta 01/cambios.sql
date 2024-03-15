71fe8596d1a6f331c38f6e573eccd3801d832674
darkaiba1234 <163485887+darkaiba1234@users.noreply.github.com>
2024-03-14 19:52:16 -0600

    Add files via upload

---
+++ carpeta 01/cambios.sql
@@ -0,0 +1,17 @@
+
+catalogo tipo de contrato
+en tipo de contrato quitar texto y poner lov
+
+mti_cxc.cxc_tipo_contrato
+pantalla principal que se muestre el tipo de contrato
+
+cambio 2 pantalla de contrato
+despues de cliente
+lista de valores de ubicacion
+*recaudacion por ubicacion
+
+en detalle de pagos al momento de generar una factura
+al generar laa factura 
+antes del metodo de pago
+lista de valores de las cajas que pertenecen
+a esa ubicacion y sean tipo virtual
